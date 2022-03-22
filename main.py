8e4e337c298fe5d7dc19a3334b266a6cccbc2cd3
Petr Machačka <91247837+PetrMachacka@users.noreply.github.com>
2022-03-22 21:30:59 +0100

    hotovo

--- main.py
+++ main.py
@@ -1,42 +1,56 @@
 radio.set_group(1)
 radio.set_transmit_power(7)
 radio.on_received_value(data_received)
-remote_serial = radio.received_packet(RadioPacketProperty.SERIAL_NUMBER)
+radio.set_transmit_serial_number(True)
 learn = 0
 alarm = 0
+data_list = [0]
+send_learn = 0
+input.on_button_pressed(Button.A, on_button_pressed_a)
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_event_pressed)
+input.on_logo_event(TouchButtonEvent.LONG_PRESSED, on_logo_event_longpressed)
+input.on_button_pressed(Button.B, on_button_pressed_b)
 
-data_list = []
 # FOREVER
 def on_forever():
-    if learn == 1:
-        pass
-basic.forever(on_forever)
-# I DON´T KNOW
-def data_received(name, value):
-    control.in_background(onIn_background)
+    pass
+# Main
 
-radio.set_transmit_serial_number(True)
-# LONG PRESS
+def data_received(name, value):
+    global data_list, alarm
+    remote_serial = radio.received_packet(RadioPacketProperty.SERIAL_NUMBER)
+    if learn == 1 and name == "send" and value == 1:
+        if remote_serial not in data_list:
+            data_list.append(remote_serial)
+            print(data_list)
+    if remote_serial in data_list and name == "alarm" and value == 1:
+        music.play_tone(Note.C, 0)
+    if remote_serial in data_list and name == "alarm" and value == 0:
+        music.stop_all_sounds()
+
+
+
+
+# LEARN
 def on_logo_event_longpressed():
     global learn
-    learn == 1
-input.on_logo_event(TouchButtonEvent.LONG_PRESSED, on_logo_event_longpressed)
-# SHORT PRESS
+    if learn == 0:
+        learn = 1
+    else:
+        learn = 0
+    print(learn)
+
+# SEND_LEARN
 def on_logo_event_pressed():
-    global learn
-    learn = 0
-input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_event_pressed)
+    radio.send_value("send", 1)
+
 # ALARM ON
 def on_button_pressed_a():
-    global alarm
-    alarm = 1
-    radio.send_value("name", 1)
-input.on_button_pressed(Button.A, on_button_pressed_a)
+
+    radio.send_value("alarm", 1)
+
 # ALARM OFF
 def on_button_pressed_b():
     global alarm
     alarm = 0
-input.on_button_pressed(Button.B, on_button_pressed_b)
-def onIn_background():
-    music.play_tone(Note.C, 3000)   
-control.in_background(onIn_background)
+    radio.send_value("alarm", 0)
--- main.ts
+++ main.ts
@@ -1,44 +1,58 @@
 radio.setGroup(1)
 radio.setTransmitPower(7)
 radio.onReceivedValue(function data_received(name: string, value: number) {
-    control.inBackground(onIn_background)
-})
-let remote_serial = radio.receivedPacket(RadioPacketProperty.SerialNumber)
-let learn = 0
-let alarm = 0
-let data_list = []
-//  FOREVER
-basic.forever(function on_forever() {
-    if (learn == 1) {
+    
+    let remote_serial = radio.receivedPacket(RadioPacketProperty.SerialNumber)
+    if (learn == 1 && name == "send" && value == 1) {
+        if (data_list.indexOf(remote_serial) < 0) {
+            data_list.push(remote_serial)
+            console.log(data_list)
+        }
         
     }
     
+    if (data_list.indexOf(remote_serial) >= 0 && name == "alarm" && value == 1) {
+        music.playTone(Note.C, 0)
+    }
+    
+    if (data_list.indexOf(remote_serial) >= 0 && name == "alarm" && value == 0) {
+        music.stopAllSounds()
+    }
+    
 })
-//  I DON´T KNOW
 radio.setTransmitSerialNumber(true)
-//  LONG PRESS
+let learn = 0
+let alarm = 0
+let data_list = [0]
+let send_learn = 0
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    radio.sendValue("alarm", 1)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_event_pressed() {
+    radio.sendValue("send", 1)
+})
 input.onLogoEvent(TouchButtonEvent.LongPressed, function on_logo_event_longpressed() {
     
-    learn == 1
-})
-//  SHORT PRESS
-input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_event_pressed() {
+    if (learn == 0) {
+        learn = 1
+    } else {
+        learn = 0
+    }
     
-    learn = 0
+    console.log(learn)
 })
-//  ALARM ON
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
-    alarm = 1
-    radio.sendValue("name", 1)
-})
-//  ALARM OFF
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
     
     alarm = 0
+    radio.sendValue("alarm", 0)
 })
-function onIn_background() {
-    music.playTone(Note.C, 3000)
+//  FOREVER
+function on_forever() {
+    
 }
 
-control.inBackground(onIn_background)
+//  Main
+//  LEARN
+//  SEND_LEARN
+//  ALARM ON
+//  ALARM OFF
